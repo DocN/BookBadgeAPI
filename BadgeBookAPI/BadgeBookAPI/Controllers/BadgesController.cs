01b0da468177aed24345f4a29856bee7a2bf18e6
DocN <031247ryan@gmail.com>
2018-12-01 23:48:47 -0800

    added badge update and get apis

--- BadgeBookAPI/BadgeBookAPI/Controllers/BadgesController.cs
+++ BadgeBookAPI/BadgeBookAPI/Controllers/BadgesController.cs
@@ -62,6 +62,7 @@ namespace BadgeBookAPI.Controllers
                         newBadge.UID = model.UID;
                         _context.Badge.Add(newBadge);
                         await _context.SaveChangesAsync();
+                        response.Data = newBadge;
                         response.Message = "Successfully created badge";
                         response.Success = true;
                     }
@@ -72,6 +73,80 @@ namespace BadgeBookAPI.Controllers
                 response.Success = false;
             }
 
+            return JsonConvert.SerializeObject(response);
+        }
+
+        [EnableCors("AllAccessCors")]
+        [Authorize(Roles = "App")]
+        [HttpPut]
+        public async Task<string> updateBadge([FromBody] UpdateBadgeViewModel model)
+        {
+            Badge newBadge = new Badge();
+            APIResponse response = new APIResponse();
+            try
+            {
+                var tokenClaims = User.Claims.Select(c =>
+                    new
+                    {
+                        Type = c.Type,
+                        Value = c.Value
+                    });
+
+                var username = tokenClaims.Where(c => c.Type.Equals(NAME_IDEN_TOKEN)).FirstOrDefault().Value;
+                var userIdent = await _userManager.FindByNameAsync(username);
+                if (userIdent != null)
+                {
+                    var currentApplication = _context.Applications.Where(c => c.UID.Equals(userIdent.Id)).FirstOrDefault();
+                    //check exist
+                    var currentbadge = _context.Badge.Where(c => c.ApplicationId.Equals(currentApplication.Id) && c.BadgeID.Equals(model.BadgeID)).FirstOrDefault();
+                    if (currentbadge != null)
+                    {
+                        currentbadge.BadgeName = model.BadgeName;
+                        currentbadge.ImageURL = model.ImageURL;
+                        currentbadge.BadgeDescription = model.BadgeDescription;
+                        _context.Badge.Update(currentbadge);
+                        await _context.SaveChangesAsync();
+                        response.Message = "Successfully updated badge";
+                        response.Success = true;
+                    }
+                    else
+                    {
+                        response.Message = "Failed to update badge ";
+                        response.Success = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                response.Message = "Failed to update badge " + e.Message;
+                response.Success = false;
+            }
+            return JsonConvert.SerializeObject(response);
+        }
+
+        [EnableCors("AllAccessCors")]
+        [HttpGet("{id}")]
+        public async Task<string> getUserBadges(string id)
+        {
+            APIResponse response = new APIResponse();
+            try
+            {
+                var userIdent = await _userManager.FindByIdAsync(id);
+                if(userIdent != null)
+                {
+                    var badgeList = _context.Badge.Where(c => c.UID.Equals(userIdent.Id)).ToList();
+                    response.Data = badgeList;
+                    response.Message = "Successfully retrieved user " + id + " badges";
+                    response.Success = true;
+                    return JsonConvert.SerializeObject(response);
+                }
+            } catch(Exception e)
+            {
+                response.Message = "failed to retrieve badges" + e.Message;
+                response.Success = false;
+            }
+            response.Message = "failed to retrieve badges" + e.Message;
+            response.Success = false;
             return JsonConvert.SerializeObject(response);
         }
         /*
